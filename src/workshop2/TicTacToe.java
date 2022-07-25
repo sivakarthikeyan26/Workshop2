f04f0dde5b710d5fb4b345d2497e1e7dec406681
Your Name <you@example.com>
2022-07-25 20:24:59 +0530

    Program to choose the symbol for the player is implemented

--- src/workshop2/TicTacToe.java
+++ src/workshop2/TicTacToe.java
@@ -1,11 +1,15 @@
 package workshop2;
 
 public class TicTacToe {
-
+	/**
+	 * 
+	 * @param args
+	 * Main function to call the method.
+	 */
 	public static void main(String[] args) {
 		TicTac_func tf = new TicTac_func();
 		tf.initialize_board();
-
+		tf.display();
+		tf.choose();
 	}
-
 }
--- src/workshop2/TicTac_func.java
+++ src/workshop2/TicTac_func.java
@@ -1,12 +1,50 @@
 package workshop2;
 
+import java.util.Scanner;
+
 public class TicTac_func {
+	/**
+	 * Creating NXN array to simulate the board
+	 */
 	char[][] board = new char[3][3];
+	public char ch, p1, p2;
+	public static Scanner sc = new Scanner(System.in);
+	/**
+	 * Function to initialize the board
+	 */
 	public void initialize_board() {
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<3; j++) {
 				board[i][j]=' ';
 			}
+		}
+	}
+	/**
+	 * Function to display the board
+	 */
+	public void display() {
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				System.out.print(board[i][j]+ " ");
+			}
+			System.out.println("");
+		}
+	}
+	/**
+	 * Fuction to let players to choose the symbol
+	 */
+	public void choose() {
+		System.out.println("Choose your symbol for the Game :\n 1.X\n2.O");
+		ch=sc.next().charAt(0);
+		switch(ch) {
+		case 1:
+			p1='X';
+			p2='O';
+			break;
+		case 2:
+			p1='O';
+			p2='X';
+			break;
 		}
 	}
 }
